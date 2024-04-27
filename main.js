fa1d0674bbe447cf5d1ab06f42e00c12d8a43d9d
Dennis Carlstrom <dennis.carlstrom@gmail.com>
2024-04-27 19:21:04 -0400

    The Vision Ahead

---
+++ main.js
@@ -0,0 +1,9 @@
+// preload code blocks that request a code url
+const preTags = document.querySelectorAll('pre[preload-code]');
+for (const pre of preTags) {
+  fetch(new Request(pre.getAttribute("preload-code")))
+    .then((response) => response.text())
+    .then((text) => {
+      pre.innerText = text;
+    });
+}
