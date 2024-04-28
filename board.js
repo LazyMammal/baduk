18e75003f86bff79051f703f5d863b19c5581d46
Dennis Carlstrom <dennis.carlstrom@gmail.com>
2024-04-28 01:22:14 -0400

    parse board

--- board.js
+++ board.js
@@ -1,3 +1,26 @@
 function board(data, output) {
-  output.text(data);
+  // split text data by row and by col
+  let array = data.split("\n")
+    .map(row => row.split(""));
+
+  // get board size from data size
+  let size = array.length;
+
+  // x-axis coordinates
+  let header = [];
+  for (let col = 0; col < size; col++) {
+    header.push(String.fromCharCode(65 + col));
+  }
+
+  // y-axis coordinates
+  for (let row = size, y = 0; row > 0; row--, y++) {
+    array[y].push(`${row}`);
+  }
+
+  // arrange board
+  let text = [header, ...array]
+    .map(row => row.join(" ")).join("\n")
+
+  // output board
+  output.text(text);
 }
