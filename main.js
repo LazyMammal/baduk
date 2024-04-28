cd9e12d84fd099d7606d3bc7ddd59536cc8611a1
Dennis Carlstrom <dennis.carlstrom@gmail.com>
2024-04-27 20:20:06 -0400

    allow preload-code on both pre and code tags

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // preload code blocks that request a code url
-const preTags = document.querySelectorAll('pre[preload-code]');
+const preTags = document.querySelectorAll(':is(code,pre)[preload-code]');
 for (const pre of preTags) {
   fetch(new Request(pre.getAttribute("preload-code")))
     .then((response) => response.text())
