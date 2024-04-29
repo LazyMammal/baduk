aa20240f71d7c89f6b718ac15932e4768a0e7da2
Dennis Carlstrom <dennis.carlstrom@gmail.com>
2024-04-28 20:04:00 -0400

    refactor again

--- board5x5.js
+++ board5x5.js
@@ -11,13 +11,20 @@ function yLabel(row) { // row -> '1','2','3'
   return `${1 + row}`;
 }
 
-function board5x5(input) {
-  let data = parse(input);
+function addAxisLabels(data) {
   let size = data.length; // board size
   data.push([]); // add row for x-axis labels
   for (let i = 0; i < size; i++) {
     data[size][i] = xLabel(i);
     data[i][size] = yLabel(i);
   }
+  return data;
+}
+
+function data2text(data) { // add whitespace, flatten array
   return data.map(row => row.join(" ")).join("\n");
+}
+
+function board5x5(input) { // final output
+  return data2text(addAxisLabels(parse(input)));
 }
