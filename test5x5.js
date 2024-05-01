a68485560ca2f5b36ab8f0a5fb1b86f1ab77b0d6
Dennis Carlstrom <dennis.carlstrom@gmail.com>
2024-04-30 22:48:20 -0400

    show source

--- test5x5.js
+++ test5x5.js
@@ -1,19 +1,20 @@
-const badge = (success) => success ? '<mark>PASS</mark>' : '<mark warn>FAIL</mark>';
+const badge = (success) => success ?
+  '<mark>PASS</mark>' : '<mark warn>FAIL</mark>';
 
 function test5x5(input) {
   let tests = extractBoards(nestedWords(input));
   let res = [];
   for (let test of tests) {
-    let data = test.boards[0]; // execute function
-    let lookup = createLookup(data);
+    let data = test.boards[0]; // test data
+    let lookup = createLookup(data); // run code
     Object.assign(lookup, createScore(data));
     for (let b = 1; b < test.boards.length; b++) {
       let label = test.labels[b];
       let board = test.boards[b];
-      let compare = [board, lookup[label]];
+      let compare = [board, lookup[label]]; // validate
       let success = `${compare[0]}` == `${compare[1]}`;
       res.push(`${badge(success)} ${label}`);
-      if (!success) {
+      if (!success) { // feedback
         res.push(parallelPrint(compare
           .map(addAxisLabels)
           .map(board => board.map(row => row.join(" ")))
@@ -24,7 +25,7 @@ function test5x5(input) {
         let compare = [extra[named], lookup[named]];
         let success = `${compare[0]}` == `${compare[1]}`;
         res.push(`${badge(success)} ${label}.${named}`);
-        if (!success) {
+        if (!success) { // feedback
           res.push(`  ${compare[0]} != ${compare[1]}`);
         }
       }
