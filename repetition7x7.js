a5960d65fc43283ef2a694847d2cfde8cd156595
Dennis Carlstrom <dennis.carlstrom@gmail.com>
2024-05-03 22:32:54 -0400

    global history

--- repetition7x7.js
+++ repetition7x7.js
@@ -1,7 +1,23 @@
 class GoRepeat extends GoLegal {
   isRepeat(board = this.board) {
-    return false; // TODO: check history
+    return hash(board) in window.baduk.history;
   }
+  playMove(x, y) {
+    super.playMove(x, y);
+    incHistory(this.board);
+  }
+}
+
+window.baduk.history = {};
+
+function hash(board) {
+  return printBoard(board, { addLabels: false })
+    .replaceAll(/[ \n]/g, "");
+}
+
+function incHistory(board) {
+  window.baduk.history[hash(board)] = 1
+    + (window.baduk.history[hash(board)] ?? 0);
 }
 
 function repetition7x7(input) {
