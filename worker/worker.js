9e0e6b33e88097dd21e6047ee5a97d8f783f79ff
Dennis Carlstrom <dennis.carlstrom@gmail.com>
2024-05-25 08:06:45 -0400

    tidy

--- worker/worker.js
+++ worker/worker.js
@@ -1,7 +1,7 @@
 self.onmessage = (event) => {
   if (Object.hasOwn(event.data, "worker")) {
-    importScripts(...event.data.worker); // first run
-    self.forkFunc = event.data.fork
+    importScripts(...event.data.worker);
+    self.forkFunc = event.data.fork;
   } else {
     self.postMessage(
       self[self.forkFunc](event.data.trim())
